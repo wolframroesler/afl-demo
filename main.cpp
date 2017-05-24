0a65d7d177ad39a60660554d7972827b82b7f70f
Wolfram Rösler <wolfram@roesler-ac.de>
2017-05-24 22:57:57 +0200

    Add header comment

--- main.cpp
+++ main.cpp
@@ -1,3 +1,9 @@
+/**
+ * @brief AFL fuzz tester demonstration program
+ * @author Wolfram Rösler <wolfram@roesler-ac.de>
+ * @date 2017-05-24
+ */
+
 #include <fstream>
 #include <iostream>
 
@@ -9,11 +15,12 @@
  *
  * @param s The URI to decode.
  * @returns The decoded URI.
+ * @bug Buggy by design.
  */
 char const *uridecode(char const *s) {
   static char ret[100];
   for(char *p=ret;*s;++s) {
-    if (*s=='%') {
+	if (*s=='%') {
 	  auto const a = *++s;
 	  auto const b = *++s;
 	  *p++ = (a<='9' ? a-'0' : a-'a') * 16 + (b<='9' ? b-'0' : b-'a');
