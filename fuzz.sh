1be6ec29ba74c9c52dc93387cfc683a65af0f71b
Wolfram Rösler <wolfram@roesler-ac.de>
2017-07-20 11:05:28 +0200

    Add some comments

--- fuzz.sh
+++ fuzz.sh
@@ -4,11 +4,11 @@
 # AFL installation path, modify as required
 AFL=~/Nextcloud/src/afl/afl-2.49b
 
+# Use AFL as a compile front-end
 export CC=$AFL/afl-clang
 export CXX=$AFL/afl-clang++
 
-set -x
-
+# Build the instrumented executable and run it under AFL
 mkdir -p aflbuild \
 && cd aflbuild \
 && cmake .. \
